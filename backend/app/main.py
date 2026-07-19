2757114b5b8d02079bd505077e289c55f5902169
yakupcalikoglu <yakupcalikoglu@hotmail.com>
2026-07-19 17:34:48 +0300

    Update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -5,7 +5,7 @@ Calistirmak icin: uvicorn app.main:app --reload
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
-from app.routers import auth, sirketler, cari, stok, siparis, banka, finansal, diger, raporlama, yetki, akreditif, virman, harcama_turu, kaynak_detay
+from app.routers import auth, sirketler, cari, stok, siparis, banka, finansal, diger, raporlama, yetki, akreditif, virman, harcama_turu, kaynak_detay,yedek_parca
 
 app = FastAPI(
     title="Kinetik ERP API",
